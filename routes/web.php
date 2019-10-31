f3a1ffa9b1ee60d32564d19b2169740b82de1f1a
chris lim <lm.chrstphr.m@gmail.com>
2019-10-31 10:32:15 +0800

    edit on product view

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ $router->group(['prefix' => 'api'], function($router)
 {
     // carousell
     $router->get('buyandsell', ['middleware' => 'cors', 'uses' => 'BuyAndSellController@mergeFrontDisplay']);
-    $router->get('buyandsellview', ['middleware' => 'cors', 'uses' => 'ScrapController@viewSingleContent']);
+    $router->get('buyandsellview', ['middleware' => 'cors', 'uses' => 'BuyAndSellController@viewSingleContent']);
     $router->get('buyandsellfeed', ['middleware' => 'cors', 'uses' => 'BuyAndSellController@feedBuyandSell']);
     $router->get('buyandsellfilter', ['middleware' => 'cors', 'uses' => 'CarousellController@filterCarousell']);
     $router->get('carousellcategory', ['middleware' => 'cors', 'uses' => 'CarousellController@loadCarousellCategory']);
