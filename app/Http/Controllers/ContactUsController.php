5b0a54965114d6ccb21e2891ff1e946c03408b8f
chris lim <lm.chrstphr.m@gmail.com>
2019-11-28 10:36:19 +0800

    update on error message

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Mailgun\Mailgun;
 use App\ContactUs;
 use Illuminate\Http\Request;
 use Mailgun\Exception\HttpClientException;
 use Buzz\Exception\NetworkException;
+use App\Http\Controllers\Functions;
 
 class ContactUsController extends Controller
 {
+    /**
+     * method to send email in  Contact Us page
+     * 
+     * @param $request
+     * @return JSON
+     */
     public function createNewContact(Request $request) {
+       $function = new Functions();
 
         // First, instantiate the SDK with your API credentials
         $mailgundata = $this->getCredential();
@@ -32,16 +41,37 @@ class ContactUsController extends Controller
             ]);
         } catch (HttpClientException $e) {
             return response()->json([
-                'message'   => $e->getMessage(),
+                'message'   => str_replace($function->getThatAnnoyingChar(), "", $e->getMessage()),
                 'result'    => true
             ]);
         } catch (NetworkException $e) {
             return response()->json([
-                'message'   => $e->getMessage(),
+                'message'   => str_replace($function->getThatAnnoyingChar(), "", $e->getMessage()),
                 'result'    => true
             ]);
         }
         
+        
+    }
+
+    /**
+     * method to validate param in contactus
+     * 
+     * @param $request
+     * @return Boolean
+     */
+    protected function validateNewContact($request) {        
+        // lets validate
+        $validator = Validator::make($request, [
+            'email'         => 'required',
+            'message'       => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return false;       
+        }else{
+            return true;
+        }
         
     }
 
